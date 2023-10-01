bcf95474e8f7dfa3162df1a11279e010f9bfcd7d
Kevin <kiovit14@gmail.com>
2023-10-01 14:27:05 +0200

    Completed single game code

--- java/script.js
+++ java/script.js
@@ -1,48 +1,40 @@
 while (true) {
-function getComputerChoice() {
-
-    let x = Math.floor(Math.random() * (4 - 1)) + 1;
-    
-    getComputerChoice = x;
-    
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function getUserChoice() {
-    let userPrompt = prompt("Rock, Paper or Scissors?");
-    if (userPrompt.toLowerCase() === "rock") {
-        return getUserChoice = 1;
+    while (true) {
+        const userPrompt = prompt("Rock, Paper or Scissors?");
+        const userChoice = userPrompt.toLowerCase();
+        
+        if (choices.includes(userChoice)) {
+            return userChoice;
+        } else {
+            alert("Please input either Rock, Paper, or Scissors");
+        }
     }
-
-    else if (userPrompt.toLowerCase() === "paper") {
-        return getUserChoice = 2;
-    }
-
-    else if (userPrompt.toLowerCase() === "scissors") {
-        return getUserChoice = 3;
-    }
-
-    else if (userPrompt.toLowerCase() != "rock", "paper", "scissors"){
-        alert("Please input either Rock, Paper or Scissors")
-    }
-
 }
 
+const choices = ["rock", "paper", "scissors"];
 const playerSelection = getUserChoice();
-const computerSelection = getComputerChoice();
+const computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
-
-    if ((getUserChoice) > (getComputerChoice)) {
-        alert("You played " + getUserChoice + " computer plays " + getComputerChoice + ". You win, VERY NICE!!")
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        alert(playerSelection + " beats " + computerSelection + " - You win, VERY NICE!!");
+    } else if (playerSelection === computerSelection) {
+        alert("It's a tie! You both chose " + playerSelection);
+    } else {
+        alert(computerSelection + " beats " + playerSelection + " - You lose :(");
     }
-    else if ((getUserChoice) < (getComputerChoice)) {
-        alert("You played " + getUserChoice + " computer plays " + getComputerChoice + ". You lose!")
-    }
-    else if ((getUserChoice) == (getComputerChoice)) {
-        alert("You played " + getUserChoice + " computer plays " + getComputerChoice + ". It's a draw!")
-    }
-
 }
 
-console.log(playRound(playerSelection, computerSelection));
+playRound(playerSelection, computerSelection);
+
 }
