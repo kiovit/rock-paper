f0b2ae9b6ff6e808bdbee48ee8ee8aa21f6fb59a
Kevin <kiovit14@gmail.com>
2023-10-02 15:29:09 +0200

    Cleaned up code

--- java/script.js
+++ java/script.js
@@ -1,13 +1,12 @@
-while (true) {
+const choices = ["rock", "paper", "scissors"];
 function computerPlay() {
-    const choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
+        return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function getUserChoice() {
     while (true) {
         const userPrompt = prompt("Rock, Paper or Scissors?");
-        const userChoice = userPrompt.toLowerCase();
+        const userChoice = userPrompt ? userPrompt.toLowerCase() : '';
         
         if (choices.includes(userChoice)) {
             return userChoice;
@@ -17,10 +16,6 @@ function getUserChoice() {
     }
 }
 
-const choices = ["rock", "paper", "scissors"];
-const playerSelection = getUserChoice();
-const computerSelection = computerPlay();
-
 function playRound(playerSelection, computerSelection) {
     if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
@@ -28,13 +23,45 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
         alert(playerSelection + " beats " + computerSelection + " - You win, VERY NICE!!");
+        return "win";       
     } else if (playerSelection === computerSelection) {
         alert("It's a tie! You both chose " + playerSelection);
+        return "tie";
     } else {
         alert(computerSelection + " beats " + playerSelection + " - You lose :(");
+        return "lose";
     }
 }
+   
+function game (roundsTotal = 5) { 
+    
+let userScore = 0;
+let computerScore = 0;
+   
+      for (let round = 1; round <= roundsTotal; round++) {
+        const playerSelection = getUserChoice();
+        const computerSelection = computerPlay();
+        const result = playRound(playerSelection, computerSelection); {
 
-playRound(playerSelection, computerSelection);
+    if (result === "win") {
+        userScore ++;
+    }
 
+    else if (result === "lose"){
+        computerScore ++;
+    }
+
+    alert(`Round ${round}/${roundsTotal} - User: ${userScore}, Computer: ${computerScore}`);
 }
+      }
+
+    if (userScore > computerScore) {
+        alert("You win the game!");
+    } else if (userScore < computerScore) {
+        alert("Computer wins the game!");
+    } else {
+        alert("It's a tie game!");
+    }
+}
+   game();
+
